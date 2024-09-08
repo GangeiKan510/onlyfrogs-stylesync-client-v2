88e67c8ff4effe4bc531f58edd844601f26596d7
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-08 21:46:32 +0800

    feat: implement tabs navigaation according to figma

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,79 +1,88 @@
-// TabLayout.tsx
 import React from "react";
 import { Tabs } from "expo-router";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import Colors from "@/constants/Colors";
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { UserProvider } from "@/components/config/user-context";
+import HomeIcon from "../../assets/icons/tabs/home.svg";
+import DesignIcon from "../../assets/icons/tabs/design.svg";
+import NotificationsIcon from "../../assets/icons/tabs/notifications.svg";
+import ProfileIcon from "../../assets/icons/tabs/profile.svg";
+import AliIcon from "../../assets/icons/tabs/ali.svg";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <UserProvider>
       <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        screenOptions={({ route }) => ({
+          tabBarInactiveTintColor: "black",
+          tabBarActiveTintColor: "black",
           headerShown: false,
-        }}
+          tabBarStyle: {
+            height: 90,
+            backgroundColor: "#F3F3F3",
+            paddingVertical: 20,
+          },
+          tabBarLabelStyle: {
+            fontSize: 14,
+            fontWeight: route.name === "index" ? "bold" : "normal",
+          },
+        })}
       >
         <Tabs.Screen
           name="home"
           options={{
-            title: "Home",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
+            title: "",
+            tabBarIcon: () => (
+              <View style={styles.iconLabelContainer}>
+                <HomeIcon width={22} height={22} />
+                <Text style={styles.tabLabel}>Home</Text>
+              </View>
             ),
           }}
         />
         <Tabs.Screen
           name="design"
           options={{
-            title: "Design",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
+            title: "",
+            tabBarIcon: () => (
+              <View style={styles.iconLabelContainer}>
+                <DesignIcon width={23.36} height={22} />
+                <Text style={styles.tabLabel}>Design</Text>
+              </View>
             ),
           }}
         />
         <Tabs.Screen
           name="index"
           options={{
-            title: "Ali",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
+            title: "",
+            tabBarIcon: () => (
+              <View style={styles.aliIconContainer}>
+                <AliIcon width={89} height={89} />
+              </View>
             ),
           }}
         />
         <Tabs.Screen
           name="notifications"
           options={{
-            title: "Notifications",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
+            title: "",
+            tabBarIcon: () => (
+              <View style={styles.iconLabelContainer}>
+                <NotificationsIcon width={22} height={22} />
+                <Text style={styles.tabLabel}>Notifications</Text>
+              </View>
             ),
           }}
         />
         <Tabs.Screen
           name="profile"
           options={{
-            title: "Profile",
-            tabBarIcon: ({ color, focused }) => (
-              <TabBarIcon
-                name={focused ? "home" : "home-outline"}
-                color={color}
-              />
+            title: "",
+            tabBarIcon: () => (
+              <View style={styles.iconLabelContainer}>
+                <ProfileIcon width={22} height={22} />
+                <Text style={styles.tabLabel}>Profile</Text>
+              </View>
             ),
           }}
         />
@@ -88,3 +97,24 @@ export default function TabLayout() {
     </UserProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  aliIconContainer: {
+    position: "absolute",
+    top: -45,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 89,
+    height: 89,
+    borderRadius: 44.5,
+  },
+  iconLabelContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabLabel: {
+    marginTop: 4,
+    fontSize: 12,
+    color: "black",
+  },
+});

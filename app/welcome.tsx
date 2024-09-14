57a825d3d6c555308e183f29762be788fac5b366
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-14 15:54:13 +0800

    feat: modify welcome page according to phone screen sizes and on figma

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,28 +1,32 @@
-import { View, Text, Pressable, ImageBackground } from "react-native";
+import { View, Text, Pressable, Dimensions } from "react-native";
 import React from "react";
 import { Href, useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { routes } from "@/utils/routes";
+import WelcomeHero from "../assets/images/svg/welcome-hero.svg";
 
 const Welcome = () => {
   const router = useRouter();
 
+  const screenHeight = Dimensions.get("window").height;
+
+  const textMarginBottom = screenHeight > 850 ? 72 : 52;
+
   return (
-    <ImageBackground
-      source={{
-        uri: "https://lh3.googleusercontent.com/pw/AP1GczMej1xjir5l5V93LJcy-yZTEt9Rd0fgwhnaDiA3hF8eKCaOyyZP4anSC9Lsmqjd7zDBIWyzzNSx4XCpXpWBNNCUeLVcljWNPKKMDegK0G3EKZB2uc67GSzfn6uUSX8PEbzZ5SsgJQ_z8pfC5zsCdCC-oiMVR78yQJLfU9OLr-w6ngkvESzXtNZxlwzNASevXg_lbM0-NwOgxMp4ntJzha6bloLf93UvVSCKOa2wHguHaw-4yK3wabt5bQFwNxzMeVlxjSGOvOlcNU3vdRqTKA7uO4gQ9oRUL5HpIqHzGKUtMaJWwEaf_oxfznYkSZe_ZiStLo0s2reHqVS0TA4UJ9NOmQ90sguC5lFjMSaAFarxxKdOnf7_JhYOs4rmqWpJVuLLxTHr5p_ds0hIS648a1CuNs8OMcocDKrJaX8kT_LyviuoXhLdJlCavtO44Y2pMgJppsaMscpctSq0S6IZ-N69SoQH4JNRXP2KrWHoYjaDdMK-wdI9-Fm9M1Dmi69aNfqZf6DE32p_B6k6FY1VElZ7XMVPIe7D_sVDl6fzVRY2qDCd5QaapFheooPF6dF6q1my5LyJdCJOUbAA7v1udsx9ACcGjaDiwAp7ylkeGmVwAJreNSDKYHEEv7v0fWdM-tRPLkxhPcYYFehCCY4SHEWzaYUYNr4rYKXtTWr4cZWs34yTk2u2aKBHpw_CrSCs-g5G8wOk7eNg_IpeLKxGF_D8QijhBbXJnLKdYv7h0J4gUhgDyqIaNLFT4y5Sb4EoKrwdmAQ59QLYbaUtf3KpIdbEE4sIqXkOpC3oLq07N2BkTVW0NIr3x_qKJjEQyu84340g1JQzkAZXVWkD2NJZTyn6be8pYwI39eM_tdLpIEkZpDe-e69KK2HRbsuhbGZ3wm7FojbiEU5G9klgsSJj02kFdBTOGFJhcXc59HFSSL29jcM8la6Z_jsCDyZQvEWpK63sZ72ev_-x6WJTHR8mM3cfXYVACH5_LX-3cxomQT6JK4m6r0vHaldRjxQSkOatUx2chuUQytTdrG_QOOMHqavbtSW71yUv0W3p=w391-h849-s-no-gm?authuser=6",
-      }}
-      className="flex-1"
-      resizeMode="cover"
-    >
-      <SafeAreaView className="flex-1">
-        <View className="flex-1 justify-center items-center">
-          <View className="mx-8 mb-28">
-            <Text className="text-[48px] text-center">StyleSync</Text>
+    <View className="flex-1 bg-white">
+      <WelcomeHero className="absolute top-0 left-0 right-0 bottom-0 z-0" />
+      <SafeAreaView className="flex-1 z-10 justify-between">
+        <View
+          className={`flex-1 justify-center items-center mb-${textMarginBottom}`}
+        >
+          <View className="items-center">
+            <Text className="text-[48px] text-center font-logo">StyleSync</Text>
             <Text className="text-[16px] text-center">Style effortlessly.</Text>
           </View>
         </View>
-        <View className="absolute bottom-12 left-0 right-0 px-8">
+
+        {/* Bottom Buttons */}
+        <View className="px-8 pb-12">
           <Pressable
             onPress={() => router.push(routes.login as Href<string | object>)}
             className="bg-[#7ab2b2] rounded-[10px] h-[48px] flex justify-center items-center mb-3"
@@ -39,7 +43,7 @@ const Welcome = () => {
           </Pressable>
         </View>
       </SafeAreaView>
-    </ImageBackground>
+    </View>
   );
 };
 

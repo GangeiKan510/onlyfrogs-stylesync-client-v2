4ca6774db03837b27f3797f258eab9f303bdf241
DaveJhaesonAlivio <97799276+DaveJhaesonAlivio@users.noreply.github.com>
2024-09-14 21:44:43 +0800

    chore: created and implemented fav colors component -> implemented logic for showing specified number of items

--- components/survey/preferences-and-budget.tsx
+++ components/survey/preferences-and-budget.tsx
@@ -1,22 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import Header from "@/components/common/Header";
 import StyleSelection from "./preferences-and-budget-components/StyleSelection";
+import FavColorSelection from "./preferences-and-budget-components/FavColorSelection";
 
 const PreferencesAndBudget = () => {
   return (
-    <View>
-      <Header/>
-      <View className="flex justify-center items-center mt-10">
-        <Text className="text-2xl">Preferences & Budget</Text>
+    <>
+      <View className="">
+        <Header />
       </View>
-      <View>
-        <View className="flex justify-start items-start mx-5 mt-10">
-          <Text className="text-lg font-bold mb-2">Choose your preferred style</Text>
-          <StyleSelection/>
+      <ScrollView>
+        <View className="flex justify-center items-center mt-10">
+          <Text className="text-2xl">Preferences & Budget</Text>
         </View>
-      </View>
-    </View>
+        <View className="flex justify-start items-start mx-5 mt-10">
+          <Text className="text-lg font-bold mb-2">
+            Choose your preferred style
+          </Text>
+          <StyleSelection />
+        </View>
+        <View className="flex justify-start items-start mx-5 mt-10">
+          <Text className="text-lg font-bold mb-2">
+            Choose your favorite color
+          </Text>
+          <FavColorSelection />
+        </View>
+      </ScrollView>
+    </>
   );
 };
 

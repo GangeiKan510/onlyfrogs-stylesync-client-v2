760a5fe45b7adedc7eed8d697fddf1b552d3ce66
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-14 15:26:04 +0800

    feat: modify layout to check for authenticated user before allowing to enter (tabs) screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tabs } from "expo-router";
 import { UserProvider } from "@/components/config/user-context";
 import HomeIcon from "../../assets/icons/tabs/home.svg";
@@ -6,9 +6,43 @@ import DesignIcon from "../../assets/icons/tabs/design.svg";
 import NotificationsIcon from "../../assets/icons/tabs/notifications.svg";
 import ProfileIcon from "../../assets/icons/tabs/profile.svg";
 import AliIcon from "../../assets/icons/tabs/ali.svg";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { Href, useRouter } from "expo-router";
+import { auth } from "@/firebaseConfig";
+import { routes } from "@/utils/routes";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function TabLayout() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoading(false);
+      } else {
+        router.replace(routes.welcome as Href<string | object>);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#F3F3F3",
+        }}
+      >
+        <ActivityIndicator size="small" color="#7ab2b2" />
+      </View>
+    );
+  }
+
   return (
     <UserProvider>
       <Tabs

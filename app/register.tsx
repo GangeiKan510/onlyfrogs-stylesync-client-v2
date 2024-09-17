d558ec8d9bae409f11d77104efd185593b30e913
yahneh <jaycellearianne.basco-21@cpu.edu.ph>
2024-09-17 20:09:49 +0800

    feat: add functionality to get user's location

--- app/register.tsx
+++ app/register.tsx
@@ -5,6 +5,7 @@ import {
   Pressable,
   KeyboardAvoidingView,
   Alert,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 import Header from "@/components/common/Header";
@@ -66,98 +67,101 @@ export default function Register() {
   };
 
   return (
-    <View className="flex-1 bg-[#ffffff]">
-      <KeyboardAvoidingView>
-        <Header />
-        <View className="mx-8 mt-16">
-          <View className="mb-4">
-            <Text className="text-[20px] font-bold">
-              Thank you for styling with us!
-            </Text>
-            <Text>Please enter your details for registration:</Text>
-          </View>
-          <View>
-            <View className="mb-3">
-              <Text className="mb-1">
-                First Name<Text className="text-[#EE4E4E]"> *</Text>
+      <View className="flex-1 bg-[#ffffff]">
+        <KeyboardAvoidingView>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <Header />
+          <View className="mx-8 mt-16">
+            <View className="mb-4">
+              <Text className="text-[20px] font-bold">
+                Thank you for styling with us!
               </Text>
-              <TextInput
-                className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
-                value={firstName}
-                onChangeText={setFirstName}
-              />
+              <Text>Please enter your details for registration:</Text>
             </View>
-            <View className="mb-3">
-              <Text className="mb-1">
-                Last Name<Text className="text-[#EE4E4E]"> *</Text>
-              </Text>
-              <TextInput
-                className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
-                value={lastName}
-                onChangeText={setLastName}
-              />
-            </View>
-            <View className="mb-3">
-              <Text className="mb-1">
-                Email<Text className="text-[#EE4E4E]"> *</Text>
-              </Text>
-              <TextInput
-                className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
-                value={email}
-                onChangeText={setEmail}
-              />
-            </View>
-            <View className="mb-3">
-              <Text>
-                Password<Text className="text-[#EE4E4E]"> *</Text>
-              </Text>
-              <TextInput
-                className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
-              />
-            </View>
-            <View className="mb-8">
-              <Text className="mb-1">
-                Confirm Password<Text className="text-[#EE4E4E]"> *</Text>
-              </Text>
-              <TextInput
-                className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                secureTextEntry={true}
-              />
-            </View>
+
             <View>
-              <CustomButton
-                isLoading={loading}
-                callBack={handleSignUp}
-                label={"Sign up"}
-                type={"primary"}
-              />
-              <Link
-                href={routes.login as Href<string | object>}
-                className="text-[#7ab2b2] underline text-center mt-3"
-              >
-                I already have an account
-              </Link>
-              <View className="flex-row items-center my-4">
-                <View className="flex-1 h-px bg-gray-400"></View>
-                <Text className="text-center mx-4">or</Text>
-                <View className="flex-1 h-px bg-gray-400"></View>
+              <View className="mb-3">
+                <Text className="mb-1">
+                  First Name<Text className="text-[#EE4E4E]"> *</Text>
+                </Text>
+                <TextInput
+                  className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
+                  value={firstName}
+                  onChangeText={setFirstName}
+                />
               </View>
-              <Pressable className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4">
-                <View className="flex-1 justify-center items-center">
-                  <Link href={routes.survey as Href<string | object>}>
-                    Sign Up with Google
-                  </Link>
+              <View className="mb-3">
+                <Text className="mb-1">
+                  Last Name<Text className="text-[#EE4E4E]"> *</Text>
+                </Text>
+                <TextInput
+                  className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
+                  value={lastName}
+                  onChangeText={setLastName}
+                />
+              </View>
+              <View className="mb-3">
+                <Text className="mb-1">
+                  Email<Text className="text-[#EE4E4E]"> *</Text>
+                </Text>
+                <TextInput
+                  className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
+                  value={email}
+                  onChangeText={setEmail}
+                />
+              </View>
+              <View className="mb-3">
+                <Text>
+                  Password<Text className="text-[#EE4E4E]"> *</Text>
+                </Text>
+                <TextInput
+                  className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={true}
+                />
+              </View>
+              <View className="mb-8">
+                <Text className="mb-1">
+                  Confirm Password<Text className="text-[#EE4E4E]"> *</Text>
+                </Text>
+                <TextInput
+                  className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4"
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  secureTextEntry={true}
+                />
+              </View>
+              <View>
+                <CustomButton
+                  isLoading={loading}
+                  callBack={handleSignUp}
+                  label={"Sign up"}
+                  type={"primary"}
+                />
+                <Link
+                  href={routes.login as Href<string | object>}
+                  className="text-[#7ab2b2] underline text-center mt-3"
+                >
+                  I already have an account
+                </Link>
+                <View className="flex-row items-center my-4">
+                  <View className="flex-1 h-px bg-gray-400"></View>
+                  <Text className="text-center mx-4">or</Text>
+                  <View className="flex-1 h-px bg-gray-400"></View>
                 </View>
-              </Pressable>
+                <Pressable className="bg-[#F3F3F3] h-[42px] rounded-[10px] px-4">
+                  <View className="flex-1 justify-center items-center">
+                    <Link href={routes.survey as Href<string | object>}>
+                      Sign Up with Google
+                    </Link>
+                  </View>
+                </Pressable>
+              </View>
             </View>
           </View>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </View>
   );
 }
--- components/survey/personal-information.tsx
+++ components/survey/personal-information.tsx
@@ -1,10 +1,31 @@
-import { View, Text, TextInput, Pressable, Platform } from "react-native";
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Platform,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import Header from "@/components/common/Header";
 import { RadioButtonProps } from "react-native-radio-buttons-group";
-import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
+import * as Location from "expo-location";
 
 const PersonalInformation = () => {
+  const [selectedId, setSelectedId] = useState<string | undefined>();
+  const [birthday, setBirthday] = useState("");
+  const [date, setDate] = useState<Date>(new Date());
+  const [show, setShow] = useState(false);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [address, setAddress] = useState<string | null>(null);
+
   const gender: RadioButtonProps[] = useMemo(
     () => [
       { id: "1", label: "Man" },
@@ -15,26 +36,29 @@ const PersonalInformation = () => {
     []
   );
 
-  const [selectedId, setSelectedId] = useState<string | undefined>();
-  const [birthday, setBirthday] = useState("");
-  const [date, setDate] = useState(new Date());
-  const [show, setShow] = useState(false);
-
   const toggleShow = () => {
     setShow(!show);
   };
 
   const formatDate = (date: Date) => {
-    const options: Intl.DateTimeFormatOptions = { month: 'long', day: '2-digit', year: 'numeric' };
-    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-    
-    const [month, day, year] = formattedDate.replace(',', '').split(' ');
-    
+    const options: Intl.DateTimeFormatOptions = {
+      month: "long",
+      day: "2-digit",
+      year: "numeric",
+    };
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+      date
+    );
+
+    const [month, day, year] = formattedDate.replace(",", "").split(" ");
+
     return `${month} | ${day} | ${year}`;
   };
-  
 
-  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+  const onChange = (
+    _event: DateTimePickerEvent,
+    selectedDate: Date | undefined
+  ) => {
     if (selectedDate) {
       setDate(selectedDate);
       setBirthday(formatDate(selectedDate));
@@ -42,92 +66,172 @@ const PersonalInformation = () => {
     setShow(false);
   };
 
+  const requestLocationPermission = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
- 
+      let locationServicesEnabled = await Location.hasServicesEnabledAsync();
+      if (!locationServicesEnabled) {
+        setErrorMsg(
+          "Location services are not enabled. Please turn on location services."
+        );
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High, 
+      });
+
+      if (location) {
+        setLocation(location);
+        setErrorMsg(null);
+
+        let reverseGeocode = await Location.reverseGeocodeAsync({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        });
+
+        if (reverseGeocode.length > 0) {
+          let firstResult = reverseGeocode[0];
+          setAddress(
+            ` ${firstResult.city}, ${firstResult.region}, ${firstResult.country}`
+          );
+        } else {
+          setErrorMsg("Unable to retrieve address from location.");
+        }
+      } else {
+        setErrorMsg("Current location not available. Please try again.");
+      }
+    } catch (error) {
+      setErrorMsg("Failed to get location: " + (error as Error).message);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (address) {
+    text = `${address}`;
+  } else if (location) {
+    text = `Coordinates: ${JSON.stringify(location.coords)}`;
+  }
+
   return (
-    <View>
+    <ScrollView>
       <Header />
-      <View>
+      <View className="flex justify-center items-center mt-10 mb-5">
         <Text className="text-[20px] text-center font-bold">
           Personal Information
         </Text>
       </View>
-      {/* Birthday */}
-      <View>
-        <Text className="pt-6 px-5 text-[16px]">Birthday</Text>
-        <View className="flex-row items-center justify-center mt-3">
-          {!show && (
-            <Pressable onPress={toggleShow}>
-              <TextInput
-                className="p-1.5 bg-[#D9D9D9] bh-[42px] rounded-[5px] px-4 w-80 text-center text-black"
-                value={birthday}
-                placeholder="Month | Day | Year"
-                editable={false}
-              />
-            </Pressable>
-          )}
-          {show && (
-            <DateTimePicker
-              mode="date"
-              display={Platform.OS === "android" ? "spinner" : "default"}
-              value={date}
-              onChange={onChange}
-            />
-          )}
-        </View>
-      </View>
-      {/* Gender */}
-      <View className="mt-12">
-        <Text className="pt-30 px-5 text-[16px]">Gender</Text>
-        <View className="px-5 flex-row flex-wrap">
-          {gender.map((button) => (
-            <View
-              key={button.id}
-              className={`border border-[#7AB2B2] rounded-full p-1 mb-4 w-[44%] ${selectedId === button.id ? "bg-[#7AB2B2]" : "bg-[#F3F3F3]"} m-2`}
-            >
-              <Text
-                className={`text-center text-base ${selectedId === button.id ? "text-white" : "text-black"}`}
-                onPress={() => setSelectedId(button.id)}
-              >
-                {button.label}
-              </Text>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }} // Added flex: 1
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+          <View>
+            {/* Birthday */}
+            <View>
+              <Text className="pt-6 px-5 text-[16px]">Birthday</Text>
+              <View className="flex-row items-center justify-center mt-3">
+                {!show && (
+                  <Pressable onPress={toggleShow}>
+                    <TextInput
+                      className="p-1.5 bg-[#D9D9D9] bh-[42px] rounded-[5px] px-4 w-80 text-center text-black"
+                      value={birthday}
+                      placeholder="Month | Day | Year"
+                      editable={false}
+                      onPressIn={toggleShow}
+                    />
+                  </Pressable>
+                )}
+                {show && (
+                  <DateTimePicker
+                    mode="date"
+                    display={Platform.OS === "android" ? "spinner" : "default"}
+                    value={date}
+                    onChange={onChange}
+                  />
+                )}
+              </View>
             </View>
-          ))}
-        </View>
-      </View>
-      {/* Height */}
-      <View className="mt-14">
-        <View className="flex-row">
-          <Text className="pt-30 px-5 text-[16px]">Height</Text>
-          <Text className="pt-25 absolute right-10 text-[20px] text-[#7AB2B2]">
-            cm
-          </Text>
-        </View>
-        <View className="items-center mt-1">
-          <TextInput
-            className="pt-1 bg-[#D9D9D9] bh-[42px] rounded-[10px] px-4 w-96 h-10"
-            keyboardType="numeric"
-            placeholder="Enter Height"
-          />
-        </View>
-      </View>
-      {/* Weight */}
-      <View className="mt-16">
-        <View className="flex-row">
-          <Text className="pt-30 px-5 text-[16px]">Weight</Text>
-          <Text className="pt-25 absolute right-10 text-[20px] text-[#7AB2B2]">
-            kg
-          </Text>
-        </View>
-        <View className="mt-1 items-center">
-          <TextInput
-            className="pt-1 bg-[#D9D9D9] bh-[42px] rounded-[10px] px-4 w-96 h-10"
-            keyboardType="numeric"
-            placeholder="Enter Weight"
-          />
-        </View>
-      </View>
-    </View>
+            {/* Gender */}
+            <View className="mt-12">
+              <Text className="pt-30 px-5 text-[16px]">Gender</Text>
+              <View className="px-5 flex-row flex-wrap">
+                {gender.map((button) => (
+                  <View
+                    key={button.id}
+                    className={`border border-[#7AB2B2] rounded-full p-1 mb-4 w-[44%] ${selectedId === button.id ? "bg-[#7AB2B2]" : "bg-[#F3F3F3]"} m-2`}
+                  >
+                    <Text
+                      className={`text-center text-base ${selectedId === button.id ? "text-white" : "text-black"}`}
+                      onPress={() => setSelectedId(button.id)}
+                    >
+                      {button.label}
+                    </Text>
+                  </View>
+                ))}
+              </View>
+            </View>
+            {/* Location */}
+            <View className="mt-11">
+              <View className="flex-row">
+                <Text className="pt-30 px-5 text-[16px]">Location</Text>
+              </View>
+            </View>
+            <View className="items-center mt-1">
+              <Pressable onPress={requestLocationPermission}>
+                <TextInput
+                  className="pt-1 bg-[#D9D9D9] bh-[42px] rounded-[10px] px-4 w-80 h-10 text-black"
+                  value={text}
+                  editable={false}
+                  onPress={requestLocationPermission}
+                />
+              </Pressable>
+            </View>
+            {/* Height */}
+            <View className="mt-11">
+              <View className="flex-row">
+                <Text className="pt-30 px-5 text-[16px]">Height</Text>
+                <Text className="pt-25 absolute right-10 text-[20px] text-[#7AB2B2]">
+                  cm
+                </Text>
+              </View>
+              <View className="items-center mt-1">
+                <TextInput
+                  className="bg-[#D9D9D9] bh-[42px] rounded-[10px] px-4 w-80 h-10"
+                  keyboardType="numeric"
+                  placeholder="Enter Height"
+                />
+              </View>
+            </View>
+            {/* Weight */}
+            <View className="mt-11">
+              <View className="flex-row">
+                <Text className="pt-30 px-5 text-[16px]">Weight</Text>
+                <Text className="pt-25 absolute right-10 text-[20px] text-[#7AB2B2]">
+                  kg
+                </Text>
+              </View>
+              <View className="mt-1 items-center">
+                <TextInput
+                  className="bg-[#D9D9D9] bh-[42px] rounded-[10px] px-4 w-80 h-10"
+                  keyboardType="numeric"
+                  placeholder="Enter Weight"
+                />
+              </View>
+            </View>
+          </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 

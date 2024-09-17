00ba0b949493c8d3bc94216b085cd86ee0aa18c5
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-18 03:54:11 +0800

    feat: add a loading indicator for the images

--- components/cards/ClothingCard.tsx
+++ components/cards/ClothingCard.tsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { View, ImageBackground } from "react-native";
+import React, { useState } from "react";
+import { View, ImageBackground, ActivityIndicator } from "react-native";
 
 interface CardProps {
   uri: string;
 }
 
 const ClothingCard: React.FC<CardProps> = ({ uri }) => {
+  const [loading, setLoading] = useState(true);
+
   return (
     <View className="w-[30%] min-w-[100px] aspect-square m-1 rounded-[10px] overflow-hidden bg-[#F3F3F3]">
       <ImageBackground
         source={{ uri: uri }}
         className="w-full h-full justify-center items-center"
         resizeMode="cover"
-      />
+        onLoad={() => setLoading(false)}
+      >
+        {loading && (
+          <ActivityIndicator
+            size="small"
+            color="#7AB2B2"
+            className="absolute"
+          />
+        )}
+      </ImageBackground>
     </View>
   );
 };

b9431666e040d132800d4fbb403eb8a6d86d0b0a
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-26 22:12:57 +0800

    chore: add dummy data for messages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import Header from "@/components/common/Header";
 import { View, ScrollView } from "react-native";
 import Bubble from "@/components/chat/bubble";
-
-const messages = [
-  { id: 1, type: "user", message: "This is a message bubble" },
-  { id: 2, type: "bot", message: "Hi I am Ali!" },
-  { id: 3, type: "user", message: "What can you do?" },
-  { id: 4, type: "bot", message: "I can help you with various tasks!" },
-];
+import { messages } from "@/components/dummy/messages";
 
 export default function HomeScreen() {
   return (

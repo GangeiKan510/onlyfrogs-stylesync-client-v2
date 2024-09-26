b30589520478d323ba93ff34bb90ba7249c8e94f
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-26 22:25:54 +0800

    feat: add floating input box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from "@/components/common/Header";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, TextInput, Pressable } from "react-native";
 import Bubble from "@/components/chat/bubble";
 import { messages } from "@/components/dummy/messages";
+import SendMessageIcon from "../../assets/icons/chat/send-icon.svg";
 
 export default function HomeScreen() {
   return (
@@ -15,6 +16,15 @@ export default function HomeScreen() {
           </View>
         ))}
       </ScrollView>
+      <View className="flex flex-row items-center h-[42px] pl-5 pr-1 bg-light-gray mx-7 mt-3 rounded-[10px] mb-7">
+        <TextInput
+          className="flex-1 h-[42px] bg-transparent"
+          placeholder="Chat with Ali..."
+        />
+        <Pressable className="h-[35px] w-[35px] bg-tertiary justify-center items-center rounded-[10px] ml-2">
+          <SendMessageIcon width={14} height={14} />
+        </Pressable>
+      </View>
     </View>
   );
 }

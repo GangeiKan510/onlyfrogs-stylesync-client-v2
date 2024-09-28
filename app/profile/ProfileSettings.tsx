738a637ad7b6d20bbcad44971f47266ec9434086
yahneh <jaycellearianne.basco-21@cpu.edu.ph>
2024-09-29 02:25:22 +0800

    fix: improve responsive UI for profile settings
    - adjusted UI for screen size adaptability
    - added state for first name, last name, and email

--- app/profile/ProfileSettings.tsx
+++ app/profile/ProfileSettings.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text, Pressable, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useState } from "react";
 import { Image, Alert } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import NoProfileImg from "../../assets/icons/profile/no-profile-img.svg";
 import CameraIcon from "../../assets/icons/profile/camera-icon.svg";
+import BackButton from "@/components/buttons/BackButton";
 
 const ProfileSettings = () => {
   const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
   const uploadProfileImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -68,79 +78,96 @@ const ProfileSettings = () => {
     // Cancel the profile settings
   };
 
+  const isFormValid = () => {
+    return (
+      firstName.trim() !== "" && lastName.trim() !== "" && email.trim() !== ""
+    );
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-[#ffffff]">
-      <View className=" flex-1 justify-center mb-11">
-        <Text className="text-center font-medium text-[16px]">
+      <View className="absolute flex-1 mt-10 mx-10">
+        <BackButton />
+      </View>
+      <View className="absolute top-10 left-0 right-0 flex items-center z-10">
+        <Text className="flex-1 text-center font-medium text-[16px]">
           Profile Settings
         </Text>
       </View>
-
-      <View className="items-center flex-1 mt-4">
-        <Pressable
-          onPress={uploadProfileImage}
-          className="absolute -bottom-[50px] z-10"
-        >
-          <View className=" h-[130px] w-[130px] rounded-full bg-[#F2F2F2] items-center justify-center ">
-            {profileImage ? (
-              <Image
-                source={{ uri: profileImage }}
-                className="h-full w-full rounded-full"
-                style={{ resizeMode: "cover" }}
-              />
-            ) : (
-              <View>
-                <NoProfileImg width={130} height={130} />
+      <KeyboardAvoidingView behavior="padding" className="flex-1">
+        <View className="items-center mt-16">
+          <Pressable onPress={uploadProfileImage} className="absolute z-10">
+            <View className="h-[130px] w-[130px] rounded-full bg-[#F2F2F2] items-center justify-center">
+              {profileImage ? (
+                <Image
+                  source={{ uri: profileImage }}
+                  className="h-full w-full rounded-full"
+                  style={{ resizeMode: "cover" }}
+                />
+              ) : (
+                <View>
+                  <NoProfileImg width={130} height={130} />
+                </View>
+              )}
+              <View className="absolute right-0 bottom-0">
+                <CameraIcon width={30} height={30} />
               </View>
-            )}
-            <View className="absolute right-0 bottom-0">
-              <CameraIcon width={30} height={30} />
             </View>
+          </Pressable>
+        </View>
+
+        <View className="mx-5 mt-40 flex-grow">
+          <Text className="text-[14px] mx-2 text-[#B7B7B7]">First Name</Text>
+          <View className="bg-[#F2F2F2] border border-[#7AB2B2] rounded-[10px] mt-2 mb-8 w-[95%] h-[40px] mx-auto">
+            <TextInput
+              placeholder="First Name"
+              className="px-5 py-2 w-full h-full"
+              value={firstName}
+              onChangeText={setFirstName}
+            />
           </View>
-        </Pressable>
-      </View>
 
-      {/* First Name */}
-      <View className=" flex mx-5 mt-20  ">
-        <Text className="font-medium text-[14px] mb-2 text-[#B7B7B7]">
-          First Name
-        </Text>
-        <TextInput className="items-center bg-[#F9F9F9] mx-auto w-[325px] h-[42px] rounded-[10px] px-5 border border-solid border-[#7AB2B2]" />
-      </View>
+          <Text className="text-[14px] mx-2 text-[#B7B7B7]">Last Name</Text>
+          <View className="bg-[#F2F2F2] border border-[#7AB2B2] rounded-[10px] mt-2 mb-8 w-[95%] h-[40px] mx-auto">
+            <TextInput
+              placeholder="Last Name"
+              className="px-5 py-2 w-full h-full"
+              value={lastName}
+              onChangeText={setLastName}
+            />
+          </View>
 
-      <View className=" flex mx-5 mt-10">
-        <Text className="font-medium text-[14px] mb-2 text-[#B7B7B7]">
-          Last Name
-        </Text>
-        <TextInput className="items-center bg-[#F9F9F9] mx-auto w-[325px] h-[42px] rounded-[10px] px-5 border border-solid border-[#7AB2B2]" />
-      </View>
+          <Text className="text-[14px] mx-2 text-[#B7B7B7]">Email</Text>
+          <View className="bg-[#F2F2F2] border border-[#7AB2B2] rounded-[10px] mt-2 w-[95%] h-[40px] mx-auto">
+            <TextInput
+              placeholder="Email"
+              className="px-5 py-2 w-full h-full"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
 
-      <View className=" flex mx-5  mt-10">
-        <Text className="font-medium text-[14px] mb-2 text-[#B7B7B7]">
-          Email
-        </Text>
-        <TextInput className="items-center bg-[#F9F9F9] mx-auto w-[325px] h-[42px] rounded-[10px] px-5 border border-solid border-[#7AB2B2]" />
-      </View>
-
-      <View className="flex-row justify-between mx-auto mb-2 ">
-        <Pressable
-          onPress={handleCancel}
-          className="flex mx-2 mt-40 bg-[#F9F9F9] rounded-[10px] border border-solid border-[#7AB2B2]"
-        >
-          <Text className="text-center text-[#7AB2B2] text-[16px]  w-[160px] h-[42px] py-2">
-            Cancel
-          </Text>
-        </Pressable>
-
-        <Pressable
-          onPress={handleSave}
-          className=" bg-[#7AB2B2] mx-2 mt-40 ml-auto rounded-[10px] border border-solid border-[#7AB2B2] "
-        >
-          <Text className="text-center text-[16px] text-white  px-auto w-[160px] h-[42px] px-4 py-2 ">
-            Save
-          </Text>
-        </Pressable>
-      </View>
+          <View className="flex-row justify-between absolute bottom-5 left-0 right-0">
+            <Pressable
+              onPress={handleCancel}
+              className="flex mx-2 bg-[#F9F9F9] rounded-[10px] border border-solid border-[#7AB2B2] w-[45%] h-[42px]"
+            >
+              <Text className="text-center text-[#7AB2B2] text-[16px] py-2">
+                Cancel
+              </Text>
+            </Pressable>
+            <Pressable
+              onPress={handleSave}
+              disabled={!isFormValid()} 
+              className={`flex mr-2 bg-[#7AB2B2] rounded-[10px] w-[45%] h-[42px] ${isFormValid() ? "bg-[#7AB2B2]" : "bg-[#B7B7B7]"}`}
+            >
+              <Text className="text-center text-[#FFFFFF] text-[16px] py-2">
+                Save
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };

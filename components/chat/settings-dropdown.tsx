74748d8e215406244fd63cbb89f970e1d568a4bb
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-29 23:54:23 +0800

    feat: add clear conversation action in settings

--- components/chat/settings-dropdown.tsx
+++ components/chat/settings-dropdown.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { View, Text, Pressable } from "react-native";
+import ClearConversationIcon from "../../assets/icons/chat/clear-conversation-icon.svg";
 
 const SettingsDropdown = ({ visible, onClose }: any) => {
   if (!visible) return null;
@@ -13,9 +14,10 @@ const SettingsDropdown = ({ visible, onClose }: any) => {
         style={{ backgroundColor: "transparent" }}
       />
 
-      <View className="absolute right-12 top-24 z-50 bg-white p-4 rounded-lg shadow">
-        <Pressable>
-          <Text className="text-black">Settings</Text>
+      <View className="absolute right-12 top-24 z-50 bg-white p-2 rounded-lg shadow">
+        <Pressable className="flex-row items-center">
+          <ClearConversationIcon />
+          <Text className="text-[#FE3B3B] ml-1">Clear conversation</Text>
         </Pressable>
       </View>
     </>

7a63b6d52b6e108ea5f657915430affcd0ce4602
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-10-09 20:48:35 +0800

    feat: initialize personal information page

--- app/(tabs)/profile/personal-information.tsx
+++ app/(tabs)/profile/personal-information.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Href, useRouter } from "expo-router";
+import { routes } from "@/utils/routes";
+import Back from "../../../assets/icons/back-icon.svg";
 
 function PersonalInformation() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-3xl">Personal Information</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="w-full flex-row items-center top-10 px-6 z-30">
+        <TouchableOpacity
+          onPress={() => router.push(routes.profile as Href<string | object>)}
+          className="absolute left-6 z-40"
+        >
+          <Back width={20} height={20} />
+        </TouchableOpacity>
+        <Text className="flex-1 text-center text-[20px] font-bold">
+          Personal Information
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 

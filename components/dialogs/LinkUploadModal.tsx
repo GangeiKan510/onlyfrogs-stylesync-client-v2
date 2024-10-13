839688ce38dd2f679dbe32617fdb22e9a6557285
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-10-13 20:21:27 +0800

    feat: update more info icon and enhance image link upload modal

--- components/dialogs/LinkUploadModal.tsx
+++ components/dialogs/LinkUploadModal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Modal, View, Text, TextInput, Pressable } from "react-native";
+import MoreInfoIcon from "../../assets/icons/more-info-icon.svg";
 
 type LinkUploadModalProps = {
   isVisible: boolean;
@@ -37,15 +38,28 @@ const LinkUploadModal: React.FC<LinkUploadModalProps> = ({
       visible={isVisible}
       onRequestClose={onClose}
     >
-      <View className="flex-1 justify-center items-center " style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+      <View
+        className="flex-1 justify-center items-center "
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+      >
         <View className="w-4/5 bg-white rounded-lg p-5 items-center">
-          <Text className="text-[18px] mb-5 font-bold">Upload Image Link</Text>
+          <Text className="text-[18px] mb-5 font-bold">Image Link Upload</Text>
           <TextInput
-            className="w-full border border-[#7AB2B2] rounded-[10px] p-2 mb-5"
-            placeholder="Paste your image link here"
+            className="w-full border border-[#7AB2B2] rounded-[10px] p-2"
+            placeholder="image.jpeg, image.png"
+            placeholderTextColor={"#bbbbbb"}
             value={link}
             onChangeText={setLink}
           />
+          <View className="my-3">
+            <View className="flex-row gap-1 items-center justify-center px-4">
+              <MoreInfoIcon />
+              <Text className="text-gray">
+                Please make sure that your image link format is either in PNG or
+                JPG.
+              </Text>
+            </View>
+          </View>
           <View className="flex-row justify-between w-full">
             <Pressable
               className="h-[42px] flex-1 border border-[#7ab3b3] rounded-lg mx-2 justify-center items-center"

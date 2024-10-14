de4280068ee2459fe12b9a0f8c774111ea0d6164
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-10-15 02:22:08 +0800

    feat: add package to check net info and modify layout to handle connection status

---
+++ app/no-connection.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const NoConnection = () => {
+  return (
+    <SafeAreaView>
+      <Text>NoConnection</Text>
+    </SafeAreaView>
+  );
+};
+
+export default NoConnection;

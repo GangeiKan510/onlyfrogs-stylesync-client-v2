7d13195f5ec5e5b518fc43a1bd3f30f99a9833d6
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-10-15 02:47:50 +0800

    feat: implement no connection page and add option to try again

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -68,6 +68,7 @@ export default function RootLayout() {
           options={{ headerShown: false }}
         />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="no-connection" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
 
